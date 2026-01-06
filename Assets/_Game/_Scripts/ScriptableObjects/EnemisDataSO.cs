e5c9ee8af557d5565b164bc44f7a2fa4cb0880d6
Alexeiad <103834584+Alexeiad@users.noreply.github.com>
2026-01-06 19:52:58 +0300

    -

--- Assets/_Game/_Scripts/ScriptableObjects/EnemisDataSO.cs
+++ Assets/_Game/_Scripts/ScriptableObjects/EnemisDataSO.cs
@@ -4,38 +4,19 @@ using System;
 [CreateAssetMenu(fileName = "EnemiesData", menuName = "Game Data/Enemies Data")]
 public class EnemiesDataSO : ScriptableObject
 {
-    [SerializeField] private EnemyTableRow[] _enemyRows = new EnemyTableRow[0];
-
-    public EnemyTableRow[] EnemyRows => _enemyRows;
+    [field: SerializeField] public EnemyTableRow[] EnemyRows { get; private set; }
 
     [Serializable]
     public class EnemyTableRow
     {
-        [SerializeField] private string _character = "Новый враг";
-        [SerializeField] private Vector2 _movementSpeed = Vector2.zero;
-        [SerializeField] private Vector2 _attackSpeed = Vector2.zero;
-        [SerializeField] private int _meleeDamage = 0;
-        [SerializeField] private Vector2 _shootSpeed = Vector2.zero;
-        [SerializeField] private int _shootDamage = 0;
-        [SerializeField] private int _health = 100;
-        [SerializeField] private int _armor = 0;
-
-        public string Character => _character;
-        public Vector2 MovementSpeed => _movementSpeed;
-        public Vector2 AttackSpeed => _attackSpeed;
-        public int MeleeDamage => _meleeDamage;
-        public Vector2 ShootSpeed => _shootSpeed;
-        public int ShootDamage => _shootDamage;
-        public int Health => _health;
-        public int Armor => _armor;
-
-        public void SetCharacter(string value) => _character = value;
-        public void SetMovementSpeed(Vector2 value) => _movementSpeed = value;
-        public void SetAttackSpeed(Vector2 value) => _attackSpeed = value;
-        public void SetMeleeDamage(int value) => _meleeDamage = value;
-        public void SetShootSpeed(Vector2 value) => _shootSpeed = value;
-        public void SetShootDamage(int value) => _shootDamage = value;
-        public void SetHealth(int value) => _health = value;
-        public void SetArmor(int value) => _armor = value;
+        
+        [field: SerializeField] public string Character { get; set; }
+        [field: SerializeField] public Vector2 MovementSpeed { get; set; } 
+        [field: SerializeField] public Vector2 AttackSpeed { get; set; }
+        [field: SerializeField] public Vector2 ShootSpeed { get; set; }
+        [field: SerializeField] public int ShootDamage { get; set; }
+        [field: SerializeField] public int MeleeDamage { get; set; }
+        [field: SerializeField] public int Health { get; set; }
+        [field: SerializeField][field: Range(0, 1)] public float Armor { get; set; }
     }
 }
