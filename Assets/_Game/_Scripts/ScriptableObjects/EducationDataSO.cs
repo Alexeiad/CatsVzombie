aceb90de02da8231a7d854137e2da17b2bd99a62
Alexeiad <103834584+Alexeiad@users.noreply.github.com>
2026-02-18 11:03:26 +0300

    -

---
+++ Assets/_Game/_Scripts/ScriptableObjects/EducationDataSO.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class EducationDataItem
+{
+    [TextArea]
+    public string text;
+    public float timeBefore;
+    public EducationTheme theme;
+}
+
+
+
+[CreateAssetMenu(fileName ="EducatonData")] 
+public class EducationDataSO : ScriptableObject
+{
+   public List<EducationDataItem> data;
+}
+public enum EducationTheme 
+{
+    Controls,
+    Shoot,
+    Spawn,
+    Clear,
+    Building,
+    BuildPoints,
+    BuildButton,
+    Food,
+    Water,
+    Production,
+    Resources,
+    Health,
+    Zombie,
+    ZombieCount
+}
+
