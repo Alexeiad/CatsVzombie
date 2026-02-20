8f4b36b8ddbecdb13f639f0149c3459af9a38678
Alexeiad <103834584+Alexeiad@users.noreply.github.com>
2026-02-20 13:46:02 +0300

    -

--- Assets/_Game/_Scripts/Game/Story/StoryBackgroundSlides.cs
+++ Assets/_Game/_Scripts/Game/Story/StoryBackgroundSlides.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class StoryBackgroundSlides : MonoBehaviour
 {
@@ -15,90 +17,53 @@ public class StoryBackgroundSlides : MonoBehaviour
     [Header("Auto")]
     [SerializeField] private float _autoNextDelay = 3f;
 
-    /// <summary>
-    /// —обытие после последнего слайда (после последнего клика/автопролистывани€).
-    /// Ќазначать извне: story.OnFinished += ...
-    /// </summary>
     public UnityAction OnFinished;
 
-    private int _index;
-    private float _lastInteractionTime;
-    private bool _finished;
-
-    // UnityAction без UnityEngine.Events (как ты и попросил)
     public delegate void UnityAction();
+
+    private int _index;
+
+    public void NextSlide()
+    {
+        _targetImage.sprite = NextSprite();
+    }
+    public void StartWith(int startIndex = 0)
+    {
+
+        Time.timeScale = 0;
+        _canvasStory.SetActive(true);
+        _targetImage.sprite = _slides[startIndex];
+        StartCoroutine(NextTo());
+    }
 
     private void Start()
     {
-        // Start запускает публичный метод, который стартует показ
-        StartShow();
-        ShowNext();
-        Time.timeScale = 0;
+        StartWith();
     }
-
-    private void Update()
+    
+    private IEnumerator NextTo()
     {
-        if (_finished) return;
-
-        // автопролистывание если давно не нажимали
-        if (Time.unscaledTime - _lastInteractionTime > _autoNextDelay)
+       
+        while (true)
         {
-            ShowNext();
-            _index++;
-            _lastInteractionTime = Time.unscaledTime; // сбрасываем таймер, чтобы не листало каждый кадр
+            _targetImage.sprite = NextSprite();
+            yield return new WaitForSecondsRealtime(_autoNextDelay);
+            
         }
     }
-    private void OnDisable()
+    private Sprite NextSprite() =>
+    
+         (_index < _slides.Count) ? _slides[_index++] : SpriteFinish();
+    
+    
+    private Sprite SpriteFinish()
     {
-        Time.timeScale = 1f;
-    }
-    /// <summary>
-    /// ѕубличный метод старта (можно дергать извне).
-    /// </summary>
-    public void StartShow()
-    {
-        _finished = false;
-        _index = 0;
-        _lastInteractionTime = Time.unscaledTime;
-        _canvasStory.SetActive(true);
-        ShowNext(); // показать первый слайд
-    }
+        Time.timeScale = 1;
+        _canvasStory.SetActive(false); 
+        _targetImage.sprite = _slides.First();
 
-    /// <summary>
-    /// ѕубличный метод дл€ клика/тапа.
-    /// ѕодключи к кнопке OnClick или EventTrigger PointerDown.
-    /// </summary>
-    public void PointerDown()
-    {
-        if (_finished) return;
-
-        _lastInteractionTime = Time.unscaledTime;
-        ShowNext();
-    }
-
-    /// <summary>
-    /// ѕоказ следующего слайда. ≈сли слайды закончились Ч вызывает OnFinished.
-    /// </summary>
-    public void ShowNext()
-    {
-        if (_finished) return;
-
-        if (_index == _slides.Count)
-        {
-            Finish();
-            return;
-        }
-
-        _targetImage.sprite = _slides[_index];
-
-        
-    }
-
-    private void Finish()
-    {
-        _finished = true;
-        Time.timeScale = 1f;
-        _canvasStory.SetActive(false);
         OnFinished?.Invoke();
+        return null;
     }
+
 }
